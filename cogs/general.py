ec8bc7641fef1dbd4113cebb9bcadd0150ccff83
Arnav Borborah <arnavborborah11@gmail.com>
2019-03-20 20:53:21 +0000

    Fix bug with bad request

--- cogs/general.py
+++ cogs/general.py
@@ -4,32 +4,37 @@ import discord
 from discord.ext import commands
 
 
+# Neon Green-ish
+HELP_EMBED_COLOR = 0x009700
+
+
 class General(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.command()
     async def hello(self, ctx, *names: str):
-        """Say hello to someone!"""
+        """Say hello to someone (or something)!"""
         await ctx.send(f"Hello, {' '.join(names)}!")
 
-    @commands.command(aliases=["commands"], description="Help")
-    async def help(self, ctx, command: Optional[str] = None):
+    @commands.command(aliases=["commands"])
+    async def help(self, ctx, help_for: Optional[str] = None):
+        """Get help for one or more commands"""
 
         help_embed = discord.Embed(title="Help", color=0x009700)
         help_embed.set_thumbnail(url=self.bot.user.avatar_url)
+
         cogs = self.bot.cogs.keys()
 
-        if command is None:
+        if help_for is None:
             for cog in cogs:
                 cog_commands = self.bot.get_cog(cog).get_commands()
+
                 commands_list = "\n".join(
-                    f"{c.name} - {c.description}" for c in cog_commands
+                    f"**{command.name}** - {command.short_doc}" for command in cog_commands
                 )
 
-                print(commands_list)
-
-                help_embed.add_field(name=cog, value="Bob", inline=False)
+                help_embed.add_field(name=cog, value=commands_list, inline=False)
 
         await ctx.send(embed=help_embed)
 
--- cogs/moderator.py
+++ cogs/moderator.py
@@ -5,6 +5,10 @@ class Moderator(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.command()
+    async def mute(self, ctx):
+        await ctx.send("Can't do it yet")
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(Moderator(bot))
