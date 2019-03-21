b71ff10b01b11d085e9925dad6b3a444d172cfae
Arnav Borborah <arnavborborah11@gmail.com>
2019-03-21 23:08:12 +0000

    Add a hello command

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ require("dotenv").config();
 
 const client = new Discord.Client();
 
+const prefix = ">>"
+
 client.once('ready', () => {
     console.log("Ready!");
 });
 
-client.on("message", message => {
-    console.log(message.content);
+client.on("message", async message => {
+    if (message.content == `${prefix}hello`) {
+        await message.channel.send(`Hello ${message.author.username}`);
+    }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN)
