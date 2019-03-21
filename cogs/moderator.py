6912705fed2d9cbdad6afbc7aca70e4082e14210
Arnav Borborah <arnavborborah11@gmail.com>
2019-03-21 10:10:41 +0000

    Mostly fix mute and unmute commands

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -4,20 +4,12 @@ import discord
 from discord.ext import commands
 
 
-def can_mute():
-    async def predicate(ctx):
-        permissions = ctx.author.permissions_in(ctx.channel)
-        return permissions.mute_members or permissions.administrator
-
-    return commands.check(predicate)
-
-
 class Moderator(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.command()
-    @can_mute()
+    @commands.has_permissions(manage_roles=True)
     async def mute(
         self, ctx, member: Optional[discord.Member] = None, reason: Optional[str] = None
     ):
@@ -31,12 +23,17 @@ class Moderator(commands.Cog):
             await ctx.send("You want to mute yourself? Uhhhhhhhhh")
             return
 
-        await member.add_roles("Muted", reason)
+        muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
+        await member.add_roles(muted_role, reason=reason)
 
-        await ctx.send(f"Muted user {member.name} for {reason}.")
+        output = f"Muted user {member.name}"
+        if reason is not None:
+            output += f" for {reason}"
+
+        await ctx.send(output)
 
     @commands.command()
-    @can_mute()
+    @commands.has_permissions(manage_roles=True)
     async def unmute(self, ctx, member: Optional[discord.Member] = None):
         """Unmute a user"""
 
@@ -48,13 +45,17 @@ class Moderator(commands.Cog):
             await ctx.send("Wait. If you're muted, then how did you run this command?")
             return
 
-        await member.remove_roles("Muted")
+        muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
+        await member.remove_roles(muted_role)
 
         await ctx.send(f"Unmuted user {member.name}.")
 
     async def cog_command_error(self, ctx, error):
+        # TODO: More robust error handling
         if isinstance(error, commands.CheckFailure):
-            await ctx.send(f"Sorry! You lack permissions to run this command!")
+            await ctx.send(
+                "Sorry! You lack permissions to run this command!\n(Or the command doesn't make any sense)"
+            )
 
 
 def setup(bot: commands.Bot):
