990f5f10dc1be81b3a7306a6ae6e87c61b657746
Arnav Borborah <arnavborborah11@gmail.com>
2019-03-29 18:18:33 +0000

    Add command to get bot URL

---
+++ src/commands/general/authorize.ts
@@ -0,0 +1,7 @@
+export const name = "url";
+export const description = "Get the authorization URL for this bot";
+export const group = "General";
+
+export async function execute(message: import('discord.js').Message, _commandArgs: string[]) {
+    await message.channel.send("https://discordapp.com/oauth2/authorize?&client_id=557603685254037504&scope=bot&permissions=8");
+}
