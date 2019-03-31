016b30367d92242a8a99ff013098f880f034799b
Arnav Borborah <arnavborborah11@gmail.com>
2019-03-31 17:07:04 -0400

    Add returns annotation

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@
  * Extract the #'s from a mention of the form <@[!]##################> (!
  * is optional)
  * @param mention The mention to extract the ID from
+ * @returns the extracted ID if found else undefined
  */
 export function extractIDFromMention(mention: string) {
   const mentionRegex = /^<@!?(\d+)>$/;
