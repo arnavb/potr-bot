4c76d42ca473e9ff644b47409c3c0631473bd23c
Arnav Borborah <arnavborborah11@gmail.com>
2019-04-01 15:51:48 -0400

    Switch to guild.ban from member.ban

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -16,19 +16,14 @@ export async function execute(message: import('discord.js').Message, commandArgs
     return;
   }
 
-  const memberToBan = message.guild.member(memberString);
-
-  if (!memberToBan) {
-    await message.channel.send("That user isn't in this server or does not exist!");
-    return;
-  }
-
   let reason = 'no reason';
 
   if (commandArgs.length > 1) {
     reason = commandArgs.slice(1).join(' ');
   }
 
-  await memberToBan.ban(reason);
-  await message.channel.send(`Successfully banned ${memberToBan} for ${reason}`);
+  await message.guild.ban(memberString, reason);
+  await message.channel.send(`Successfully banned for ${reason}!`);
+  // TODO: Figure out a way to get this to work --v
+  // memberToBan.send(`Hey! You were banned from ${message.guild.name} for ${reason}`);
 }
