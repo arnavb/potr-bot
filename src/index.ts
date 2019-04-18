c9dec3e13c65332c10b2c203b6a83b0d02651a90
Arnav Borborah <arnavborborah11@gmail.com>
2019-04-18 15:29:27 -0400

    Remove an unnecessary variable

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import { config } from 'dotenv';
 import { Bot } from './bot';
 config();
 
-const potrBot = new Bot({
+new Bot({
   prefix: '>>',
   discordBotToken: process.env.DISCORD_BOT_TOKEN!,
   postgresDbUri: process.env.POSTGRES_DB_URI!,
-});
-
-potrBot.start();
+}).start();
